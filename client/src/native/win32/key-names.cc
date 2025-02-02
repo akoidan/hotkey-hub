58f1e96c22ad97ac7ce7f1b3118b5a5dd2f95e8a
akoidan <deathangel908@gmail.com>
2025-02-02 17:37:47 +0100

    Remove nutjs

--- client/src/native/win32/key-names.cc
+++ client/src/native/win32/key-names.cc
@@ -6,6 +6,13 @@ typedef struct {
 } KeyNames;
 
 static KeyNames key_names[] = {
+    {"numpad_decimal",           VK_DECIMAL},
+    {"enter",            VK_RETURN},
+    {"clear",            VK_CLEAR},
+    {"add",          VK_ADD},
+    {"subtract",             VK_SUBTRACT},
+    {"multiply",             VK_MULTIPLY},
+    {"divide",           VK_DIVIDE},
     {"backspace",         VK_BACK},
     {"delete",           VK_DELETE},
     {"return",           VK_RETURN},
