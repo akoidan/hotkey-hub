2234fd0317b91d6c3bddb1f73eadd7b1b16aff8c
death <deathangel908@gmail.com>
2025-06-26 13:42:53 +0200

    as

--- src/rgb/rgb-service.ts
+++ src/rgb/rgb-service.ts
@@ -100,6 +100,6 @@ export class RgbService {
   }
 
   private encodeKey(led: { name: string; value: { red: any; green: any; blue: any } }): string {
-    return led.name.toLowerCase().replace('key: ', '').replace(' (ansi)', '').replace(' ', '_');
+    return led.name.toLowerCase().replace('arrow', '').replace('key: ', '').replace(' (ansi)', '').replace(' ', '_');
   }
 }
