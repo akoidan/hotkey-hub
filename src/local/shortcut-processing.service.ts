b5f7d15fe2389cd74c9b88fbc2d859dc25cf3cf7
death <deathangel908@gmail.com>
2025-08-17 13:52:06 +0200

    d

--- src/local/shortcut-processing.service.ts
+++ src/local/shortcut-processing.service.ts
@@ -47,7 +47,9 @@ export class ShortcutProcessingService {
 
 
   private async runRestartableProcess(comb: Shortcut, id: string): Promise<void> {
-    const running = this.iterationsInProgress[comb.shortCut].map(proc => proc.status).filter(s => s === ProcessStatus.RUNNING);
+    const running = this.iterationsInProgress[comb.shortCut]
+      .map(proc => proc.status)
+      .filter(s => s === ProcessStatus.RUNNING);
     if (running.length >0) {
       this.logger.debug(`Stopping ${running.join(',')} instances of ${clc.bold.green(comb.name)}`);
       this.iterationsInProgress[comb.shortCut]
