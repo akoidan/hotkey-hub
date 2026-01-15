883a3679e3337d32473c8e1975051e2718cab090
death <deathangel908@gmail.com>
2026-01-15 09:42:53 +0100

    dddd

--- src/remote/implementation/key-press-remote-handler.ts
+++ src/remote/implementation/key-press-remote-handler.ts
@@ -1,9 +1,10 @@
 import {RandomService} from '@/random/random-service';
 import {ClientService} from '@/client/client-service';
 import {Injectable} from '@nestjs/common';
-import {Key, KeyPressRemoteCommand} from '@/config/types/remote/keyboard-commands-schema';
+import {Key, KeyPressRemoteCommand, KeyPressRemoteVariable} from '@/config/types/remote/keyboard-commands-schema';
 import {RemoteCommand} from '@/config/types/remote/remote-commands';
 import {CommandRemoteHandler} from '@/remote/command-remote-handler';
+import {SendKeyRequest} from '@/client/dtos';
 
 @Injectable()
 export class KeyPressRemoteHandler extends CommandRemoteHandler {
@@ -19,21 +20,23 @@ export class KeyPressRemoteHandler extends CommandRemoteHandler {
   }
 
   async execute(destination: string, command: KeyPressRemoteCommand): Promise<void> {
+    const vars = command.variables as KeyPressRemoteVariable;
+    let duration: number|undefined = vars.duration;
+    if (vars.duration && vars.durationDeviation) {
+      duration = this.randomService.calcDeviation(vars.duration as number, vars.durationDeviation as number);
+    }
     let holdKeys: Key[] = [];
-    if (Array.isArray(command.variables.holdKeys)) {
-      holdKeys = command.variables.holdKeys as Key[];
-    } else if (command.variables.holdKeys) {
-      holdKeys = [command.variables.holdKeys as Key];
+    if (Array.isArray(vars.holdKeys)) {
+      // eslint-disable-next-line @typescript-eslint/prefer-destructuring
+      holdKeys = vars.holdKeys
+    } else if (typeof vars.holdKeys !== 'undefined') {
+      holdKeys = [vars.holdKeys]
     }
-
-    let duration: number | undefined = command.variables.duration as number| undefined;
-    if (command.variables.duration && command.variables.durationDeviation) {
-      duration = this.randomService.calcDeviation(command.variables.duration as number, command.variables.durationDeviation as number);
-    }
-    await this.clientService.keyPress(destination, {
-      keys: Array.isArray(command.variables.key) ? command.variables.key as Key[] : [command.variables.key as Key],
+    const newVars: SendKeyRequest = {
+      keys: Array.isArray(vars.key) ? vars.key : [vars.key],
       holdKeys,
       duration,
-    });
+    };
+    await this.clientService.keyPress(destination, newVars);
   }
 }
