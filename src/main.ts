203750ed7493c7bb2d025fb9ea21e557c9114068
akoidan <deathangel908@gmail.com>
2026-02-09 06:22:47 +0100

    sdf3d

--- src/main.ts
+++ src/main.ts
@@ -4,108 +4,10 @@ import {CustomLogger} from '@/app/custom-logger';
 import * as process from 'node:process';
 import {asyncLocalStorage} from '@/asyncstore/async-storage-value';
 import {SemaphorService} from '@/semaphor/semaphor-service';
-import path from 'path';
-import yargs from 'yargs';
-import type {AppConfig, ReloadRequest} from '@/app/app-model';
-import net from 'net';
-import * as http from 'http';
+import type {ReloadRequest} from '@/app/app-model';
+import {isPortOpen, parseArgs, postLocalhost} from '@/app/utils';
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-async function parseArgs(): Promise<AppConfig> {
-  const isNodeJs = process.execPath.endsWith('node') || process.execPath.endsWith('node.exe');
-  const commonDir = isNodeJs ? process.cwd() : path.dirname(process.execPath);
 
-  return yargs(process.argv.slice(2))
-      .strict()
-      .scriptName('hotkey-hub')
-      .epilog('Reffer https://github.com/akoidan/hotkey-hub for more documentation')
-      .usage('Allows to control remote pc using OS hotkeys')
-      .option('config-file', {
-        type: 'string',
-        default: path.join(commonDir, 'configs', 'config.jsonc'),
-        description: 'Configs that describes hotkey bindins',
-      })
-      .option('variables-file', {
-        type: 'string',
-        default: path.join(commonDir, 'configs', 'variables.jsonc'),
-        description: 'File that used to store permanent variables across restarts',
-      })
-      .option('enable-api', {
-        type: 'boolean',
-        default: false,
-      })
-      .option('api-port', {
-        type: 'number',
-        default: 6000,
-        description: 'if enable-api activates, exposes api on this port',
-      })
-      .implies('api-port', 'enable-api')
-      .option('cert-dir', {
-        type: 'string',
-        default: path.join(commonDir, 'certs'),
-        description: 'Directory that contains key.pem, cert.pem, ca-cert.pem for MTLS',
-      })
-      .parse();
-}
-
-async function isPortOpen(port: number, timeout = 2000): Promise<boolean> {
-  return new Promise((resolve) => {
-    const socket = new net.Socket();
-
-    socket.setTimeout(timeout);
-    socket.on('connect', () => {
-      socket.destroy();
-      resolve(true); // port is open
-    });
-    socket.on('timeout', () => {
-      socket.destroy();
-      resolve(false); // port closed or unreachable
-    });
-    socket.on('error', () => {
-      resolve(false); // port closed
-    });
-
-    socket.connect(port, '127.0.0.1');
-  });
-}
-
-function postReload(body: any, port: number): Promise<any> {
-  return new Promise((resolve, reject) => {
-    const data = JSON.stringify(body);
-
-    const options: http.RequestOptions = {
-      hostname: '127.0.0.1', // use 127.0.0.1 instead of localhost
-      port: port,
-      path: '/reload',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(data),
-      },
-    };
-
-    const req = http.request(options, (res) => {
-      let responseData = '';
-      res.on('data', (chunk) => (responseData += chunk));
-      res.on('end', () => {
-        if (res.statusCode && res.statusCode >= 200 && res.statusCode < 300) {
-          resolve(responseData);
-        } else {
-          try {
-            const bodyObj = JSON.parse(responseData) as Error;
-            reject(new Error(`Unable to apply new configuration. ${bodyObj.message}`));
-          } catch (e) {
-            reject(new Error(`Unable to apply new configuration. Unkown error ${responseData}`));
-          }
-        }
-      });
-    });
-
-    req.on('error', (err) => reject(err));
-    req.write(data);
-    req.end();
-  });
-}
 
 asyncLocalStorage.run(new Map<string, string>().set(SemaphorService.COMB_KEY, 'init'), () => {
   const customLogger = new CustomLogger(asyncLocalStorage);
@@ -114,18 +16,17 @@ asyncLocalStorage.run(new Map<string, string>().set(SemaphorService.COMB_KEY, 'i
     const packageJson: string = require('../package.json').version;
     const args = await parseArgs();
     if (args.enableApi) {
-       customLogger.log(`Initializing hotkey-hub ${packageJson} ...`);
-        const app = await NestFactory.create(
-          AppModule.forRoot(args),
-          {
-            logger: customLogger,
-          }
-        );
-        customLogger.log(`Starting hotkey-hub ${packageJson} at prt ${args.apiPort}`);
-        await app.listen(args.apiPort, '127.0.0.1');
+      customLogger.log(`Initializing hotkey-hub ${packageJson} ...`);
+      const app = await NestFactory.create(
+        AppModule.forRoot(args),
+        {
+          logger: customLogger,
+        }
+      );
+      customLogger.log(`Starting hotkey-hub ${packageJson} at prt ${args.apiPort}`);
+      await app.listen(args.apiPort, '127.0.0.1');
     } else if (await isPortOpen(args.apiPort)) {
-      const body: ReloadRequest = {
-      };
+      const body: ReloadRequest = {};
       if (process.argv.some(arg => arg === '--config-file' || arg.startsWith('--config-file='))) {
         body.configFile = args.configFile;
       }
@@ -135,7 +36,7 @@ asyncLocalStorage.run(new Map<string, string>().set(SemaphorService.COMB_KEY, 'i
       if (Object.keys(body).length === 0) {
         throw new Error(`hotkey-hub is already running at port ${args.apiPort}`);
       }
-      await postReload(body, args.apiPort);
+      await postLocalhost(body, '/reload', args.apiPort);
       customLogger.log(`Applied new configuration ${JSON.stringify(body)} to already running hotkey-hub at port ${args.apiPort}`);
     } else {
       customLogger.log(`Initializing hotkey-hub ${packageJson} ...`);
