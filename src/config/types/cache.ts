5d86a1a23f40bde72a0d885854e20571214c2daf
death <deathangel908@gmail.com>
2026-05-12 00:55:05 +0200

    1122

--- src/config/types/cache.ts
+++ src/config/types/cache.ts
@@ -1,22 +1,8 @@
 import type {ConfigData} from '@/config/types/root';
-import {type ValidateFunction, Ajv} from 'ajv';
-import type {JsonSchema} from '@/config/types/local/macro-local-command';
 
-const ajv = new Ajv({strict: false, strictSchema: true, useDefaults: true});
-
-const cache = new Map<JsonSchema, ValidateFunction>();
 
 export const schemaRootCache: {
   data: ConfigData,
-  getSchema(schema: JsonSchema): ValidateFunction,
 } = {
   data: null!,
-  getSchema(schema: JsonSchema): ValidateFunction {
-    if (!cache.has(schema)) {
-      cache.set(schema, ajv.compile(schema));
-    }
-    return cache.get(schema)!;
-  },
 };
-export type AJV = typeof ajv;
-
